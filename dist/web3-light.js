6415d9b7e6acfb1765e37f861d7013bf34ab413b
lemengbin <lemengbin@163.com>
2019-05-30 18:29:43 +0800

    Hex address => Um address

--- dist/web3-light.js
+++ dist/web3-light.js
@@ -528,7 +528,7 @@ var SolidityType = require('./type');
  * address[][6][], ...
  */
 var SolidityTypeAddress = function () {
-    this._inputFormatter = f.formatInputInt;
+    this._inputFormatter = f.formatInputAddress;
     this._outputFormatter = f.formatOutputAddress;
 };
 
@@ -898,6 +898,7 @@ var BigNumber = require('bignumber.js');
 var utils = require('../utils/utils');
 var c = require('../utils/config');
 var SolidityParam = require('./param');
+var Base58 = require('../base58');
 
 
 /**
@@ -1104,10 +1105,27 @@ var formatOutputString = function (param) {
  */
 var formatOutputAddress = function (param) {
     var value = param.staticPart();
-    return "0x" + value.slice(value.length - 40, value.length);
+    return Base58.AddressToBase58Address(value.slice(value.length - 40, value.length));
+};
+
+/**
+ * Should be use to format input address
+ *
+ * @method formatInputAddress
+ * @param {String} value that must be address string('Um....')
+ * @returns {SolidityParam or Error}
+ */
+var formatInputAddress = function (value) {
+    if (utils.isAddress(value)) {
+        BigNumber.config(c.ETH_BIGNUMBER_ROUNDING_MODE);
+        var result = utils.padLeft(utils.toTwosComplement(Base58.Base58AddressToAddress(value)).toString(16), 64);
+        return new SolidityParam(result);
+    }
+    throw new Error('invalid address');
 };
 
 module.exports = {
+    formatInputAddress: formatInputAddress,
     formatInputInt: formatInputInt,
     formatInputBytes: formatInputBytes,
     formatInputDynamicBytes: formatInputDynamicBytes,
@@ -1125,7 +1143,7 @@ module.exports = {
     formatOutputAddress: formatOutputAddress
 };
 
-},{"../utils/config":18,"../utils/utils":20,"./param":11,"bignumber.js":"bignumber.js"}],10:[function(require,module,exports){
+},{"../utils/config":18,"../utils/utils":20,"./param":11,"bignumber.js":"bignumber.js","../base58":87}],10:[function(require,module,exports){
 var f = require('./formatters');
 var SolidityType = require('./type');
 
@@ -2231,7 +2249,7 @@ var toTwosComplement = function (number) {
  * @return {Boolean}
 */
 var isStrictAddress = function (address) {
-    return /^0x[0-9a-f]{40}$/i.test(address);
+    return (/^(Um)?[1-9a-z]{33}$/i.test(address)) && (!/[OIl]{1}/.test(address));
 };
 
 /**
@@ -2242,16 +2260,7 @@ var isStrictAddress = function (address) {
  * @return {Boolean}
 */
 var isAddress = function (address) {
-    if (!/^(0x)?[0-9a-f]{40}$/i.test(address)) {
-        // check if it has the basic requirements of an address
-        return false;
-    } else if (/^(0x)?[0-9a-f]{40}$/.test(address) || /^(0x)?[0-9A-F]{40}$/.test(address)) {
-        // If it's all small caps or all all caps, return true
-        return true;
-    } else {
-        // Otherwise check each case
-        return isChecksumAddress(address);
-    }
+    return (/^(Um)?[1-9a-z]{33}$/i.test(address)) && (!/[OIl]{1}/.test(address));
 };
 
 /**
@@ -2313,12 +2322,7 @@ var toAddress = function (address) {
     if (isStrictAddress(address)) {
         return address;
     }
-
-    if (/^[0-9a-f]{40}$/.test(address)) {
-        return '0x' + address;
-    }
-
-    return '0x' + padLeft(toHex(address).substr(2), 40);
+    return address;
 };
 
 /**
@@ -3916,13 +3920,8 @@ var outputPostFormatter = function(post){
 };
 
 var inputAddressFormatter = function (address) {
-    var iban = new Iban(address);
-    if (iban.isValid() && iban.isDirect()) {
-        return '0x' + iban.address();
-    } else if (utils.isStrictAddress(address)) {
+    if (utils.isAddress(address)) {
         return address;
-    } else if (utils.isAddress(address)) {
-        return '0x' + address;
     }
     throw new Error('invalid address');
 };
@@ -13610,6 +13609,128 @@ module.exports = transfer;
 
 },{}],86:[function(require,module,exports){
 module.exports = XMLHttpRequest;
+
+},{}],87:[function(require,module,exports){
+/**
+ * @file base58.js
+ * @author lemengbin <lemengbin@163.com>
+ * @date 2019
+ */
+var CryptoJS = require('crypto-js');
+var ALPHABET = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz'; // 58 characters
+var ALPHABET_MAP = {};
+for (var i = 0; i < 58; i++) {
+    ALPHABET_MAP[ALPHABET.charAt(i)] = i;
+}
+
+var Base58Encode = function (arr) {
+    if (arr.length === 0)
+        return '';
+
+    var i, j, digits = [0];
+    for (i = 0; i < arr.length; i++) {
+        for (j = 0; j < digits.length; j++) {
+            digits[j] <<= 8;
+        }
+
+        digits[0] += arr[i];
+        var carry = 0;
+        for (j = 0; j < digits.length; ++j) {
+            digits[j] += carry;
+            carry = (digits[j] / 58) | 0;
+            digits[j] %= 58;
+        }
+        while (carry) {
+            digits.push(carry % 58);
+            carry = (carry / 58) | 0;
+        }
+    }
+
+    // deal with leading zeros
+    for (i = 0; arr[i] === 0 && i < arr.length - 1; i++)
+        digits.push(0);
+    return digits.reverse().map(function(digit) { return ALPHABET[digit]; }).join('');
+};
+
+var Base58Decode = function (str) {
+    if (str.length === 0)
+        return [];
+
+    var i, j, bytes = [0];
+    for (i = 0; i < str.length; i++) {
+        var c = str[i];
+        if (!(c in ALPHABET_MAP))
+            throw new Error('Non-base58 character');
+
+        for (j = 0; j < bytes.length; j++)
+            bytes[j] *= 58;
+        bytes[0] += ALPHABET_MAP[c];
+
+        var carry = 0;
+        for (j = 0; j < bytes.length; ++j) {
+            bytes[j] += carry;
+            carry = bytes[j] >> 8;
+            bytes[j] &= 0xff;
+        }
+        while (carry) {
+            bytes.push(carry & 0xff);
+            carry >>= 8;
+        }
+    }
+
+    // deal with leading zeros
+    for (i = 0; str[i] === '1' && i < str.length - 1; i++)
+        bytes.push(0);
+
+    return bytes.reverse();
+};
+
+var BytesToString = function (arr) {
+    var str = "";
+    for (var i = 0; i < arr.length; i++) {
+        var tmp = arr[i].toString(16);
+        if (tmp.length == 1) {
+            tmp = "0" + tmp;
+        }
+        str += tmp;
+    }
+    return str;
+};
+
+var StringToBytes = function (str) {
+    var len = str.length;
+    if (len % 2 != 0) {
+        return null;
+    }
+    len /= 2;
+
+    var arr = new Array();
+    var pos = 0;
+    for (var i = 0; i < len; i++) {
+        var tmp = str.substr(pos, 2);
+        var v = parseInt(tmp, 16);
+        arr.push(v);
+        pos += 2;
+    }
+    return arr;
+};
+
+var AddressToBase58Address = function (value) {
+    value = value.toString().replace('0x','');
+    var buff = '0FA2' + value.slice(value.length - 40, value.length);
+    var hash1 = CryptoJS.SHA256(CryptoJS.enc.Hex.parse(buff));
+    var hash2 = CryptoJS.SHA256(hash1).toString();
+    return Base58Encode(StringToBytes(buff + hash2.slice(0, 8)));
+};
+
+var Base58AddressToAddress = function (value) {
+    return "0x" + BytesToString(Base58Decode(value.toString()).slice(2, 22));
+};
+
+module.exports = {
+    AddressToBase58Address: AddressToBase58Address,
+    Base58AddressToAddress: Base58AddressToAddress
+};
 
 },{}],"bignumber.js":[function(require,module,exports){
 'use strict';
